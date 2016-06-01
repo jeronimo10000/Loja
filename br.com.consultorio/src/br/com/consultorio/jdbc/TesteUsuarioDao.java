61d973a9eba26211d4ec381ecdeb377c00d81e85
jeronimo10000 <jeronimo000000@gmail.com>
2016-06-01 00:35:37 -0300

    Scanner para cadastrar paciente

--- br.com.consultorio/src/br/com/consultorio/jdbc/TesteUsuarioDao.java
+++ br.com.consultorio/src/br/com/consultorio/jdbc/TesteUsuarioDao.java
@@ -1,40 +1,55 @@
 package br.com.consultorio.jdbc;
 
+import java.util.Scanner;
+
 import br.com.consultorio.jdbc.dao.PacienteDao;
 import br.com.consultorio.model.Paciente;
 
 public class TesteUsuarioDao {
 
 	public static void main(String[] args) {
-//		// pronto para gravar
-//		Paciente paciente = new Paciente();
-//		paciente.setId(2);
-//		paciente.setNome("Mago");
-//		paciente.setRg("3302589");
-//		paciente.setCpf("021.258.365-01");
-//		paciente.setSexo("Masculino");
-//
-//		// grave nessa conexão!!!
-//		PacienteDao dao = new PacienteDao();
-//
-//		// método elegante
-//		dao.adiciona(paciente);
-//
-//		System.out.println("Gravado!");
 		
-		Paciente paciente2 = new Paciente();
-		paciente2.setId(2);
-		paciente2.setNome("Jeronimo");
-		paciente2.setRg("33044789");
-		paciente2.setCpf("025.256.356-89");
-		paciente2.setSexo("Masculino");
-		paciente2.setId(2);
+		Scanner gravar = new Scanner(System.in);
 		
-		PacienteDao dao1 = new PacienteDao();
+		System.out.println("Cadastro de Paciente");
+		System.out.println("Nome do Paciente: ");
+		String nome = gravar.nextLine();
+		System.out.println("Rg: ");
+		String rg = gravar.nextLine();
+		System.out.println("CPF: ");
+		String cpf = gravar.nextLine();
+		System.out.println("Sexo: ");
+		String sexo = gravar.nextLine();
 		
-		dao1.atualiza(paciente2);
+		// pronto para gravar
+		Paciente paciente = new Paciente();
+		paciente.setId(0);
+		paciente.setNome(nome);
+		paciente.setRg(rg);
+		paciente.setCpf(cpf);
+		paciente.setSexo(sexo);
+
+		// grave nessa conexão!!!
+		PacienteDao dao = new PacienteDao();
+
+		// método elegante
+		dao.adiciona(paciente);
+
+		System.out.println("Gravado!");
 		
-		System.out.println("Atulizado!");
+//		Paciente paciente2 = new Paciente();
+//		paciente2.setId(1);
+//		paciente2.setNome("Mago");
+//		paciente2.setRg("3365897");
+//		paciente2.setCpf("523.689.564-58");
+//		paciente2.setSexo("Masculino");
+//		paciente2.setId(1);
+//		
+//		PacienteDao dao1 = new PacienteDao();
+//		
+//		dao1.atualiza(paciente2);
+//		
+//		System.out.println("Atulizado!");
 
 	}
 }
